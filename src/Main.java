89aaed0bb04ea0c87406eba8fd99412964a8e408
Dima Zap <zapilenovd@gmail.com>
2021-12-02 21:04:27 +0300

    ints commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 25;
+        int numbertic = 7;
+        int bonus = 20;
+        int mils = ticket * numbertic / bonus;
+        System.out.println(mils);
+    }
+}
